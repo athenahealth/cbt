eed5f3f028ce5b1c03d37d1eec7f058c75c95429
Kevin Scheunemann <kevin@scheuk.com>
2015-06-29 17:55:34 +0000

    modified to work with athena nodes

--- monitoring.py
+++ monitoring.py
@@ -13,13 +13,13 @@ def start(directory):
     common.pdsh(nodes, 'collectl -s+mYZ -i 1:10 -F0 -f %s' % collectl_dir)
 
     # perf
-#    common.pdsh(nodes), 'mkdir -p -m0755 -- %s' % perf_dir).communicate()
-#    common.pdsh(nodes), 'cd %s;sudo perf_3.6 record -g -f -a -F 100 -o perf.data' % perf_dir)
+    common.pdsh(nodes, 'mkdir -p -m0755 -- %s' % perf_dir)
+    common.pdsh(nodes, 'cd %s;sudo perf record -g -f -a -F 100 -o perf.data' % perf_dir)
 
     # blktrace
-#    common.pdsh(osds, 'mkdir -p -m0755 -- %s' % blktrace_dir).communicate()
-#    for device in xrange (0,osds_per_node):
-#        common.pdsh(osds, 'cd %s;sudo blktrace -o device%s -d /dev/disk/by-partlabel/osd-device-%s-data' % (blktrace_dir, device, device))
+    common.pdsh(settings.getnodes('osds'), 'mkdir -p -m0755 -- %s' % blktrace_dir)
+    for device in 'bcdefghijklm':
+        common.pdsh(settings.getnodes('osds'), 'cd %s;sudo blktrace -o sd%s1 -d /dev/sd%s1' % (blktrace_dir, device, device))
 
 
 
@@ -27,7 +27,7 @@ def stop(directory=None):
     nodes = settings.getnodes('clients', 'osds', 'mons', 'rgws')
 
     common.pdsh(nodes, 'pkill -SIGINT -f collectl').communicate()
-    common.pdsh(nodes, 'sudo pkill -SIGINT -f perf_3.6').communicate()
+    common.pdsh(nodes, 'sudo pkill -SIGINT -f perf').communicate()
     common.pdsh(settings.getnodes('osds'), 'sudo pkill -SIGINT -f blktrace').communicate()
     if directory:
         sc = settings.cluster
@@ -36,9 +36,9 @@ def stop(directory=None):
 
 def make_movies(directory):
     sc = settings.cluster
-    seekwatcher = '/home/%s/bin/seekwatcher' % sc.get('user')
+    seekwatcher = '/usr/bin/seekwatcher'
     blktrace_dir = '%s/blktrace' % directory
 
-    for device in xrange (0,sc.get('osds_per_node')):
-        common.pdsh(settings.getnodes('osds'), 'cd %s;%s -t device%s -o device%s.mpg --movie' % (blktrace_dir,seekwatcher,device,device)).communicate()
+    for device in 'bcdefghijklm':
+        common.pdsh(settings.getnodes('osds'), 'cd %s;%s -t sd%s1 -o sd%s1.mpg --movie' % (blktrace_dir,seekwatcher,device,device)).communicate()
 
